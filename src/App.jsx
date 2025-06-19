1a793bc74cee2c6d85cb9b4b17a4f5cf43e518b2
Allieniaszxy <alakaalamin40@gmail.com>
2025-06-19 11:32:21 -1100

    stopped at the main section otherwise known as hero page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from "./components/navbar/Nav";
+import Main from "./components/main/Main";
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <Main />
+    </div>
+  );
+};
+export default App;
